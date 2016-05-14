20b8f26927767fcc96171b7d56aca593cec02b9d
Black Roland <black-roland@github.com>
2016-05-14 22:56:46 +0900

    Fix host/port parsing

--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 module.exports = {
 
   parseHost: (binary) => {
-    const matches = /teeworlds_(info|ping)_([^:$]+)/.exec(binary);
+    const matches = /teeworlds_(players|ping)_([^:$]+)/.exec(binary);
     if (!matches)
       return null;
     return matches[2];
   },
 
   parsePort: (binary) => {
-    const matches = /teeworlds_(info|ping)_[^:]+:([0-9]+)$/.exec(binary);
+    const matches = /teeworlds_(players|ping)_[^:]+:([0-9]+)$/.exec(binary);
     if (!matches)
       return null;
     return matches[2];
