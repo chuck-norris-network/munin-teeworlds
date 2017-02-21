46dfc2ce8e8230ad90a2ff6c597ba10b7b2c5f29
Roland Chernov <mail@roland.black>
2017-02-21 21:40:21 +0900

    Better ping

--- utils.js
+++ utils.js
@@ -1,27 +1,32 @@
-module.exports = {
+module.exports = {};
 
-  parseHost: (binary) => {
-    const matches = /teeworlds_(players|ping)_([^:$]+)/.exec(binary);
-    if (!matches)
-      return null;
-    return matches[2];
-  },
+module.exports.parseHost = (binary) => {
+  const matches = /teeworlds_(players|ping)_([^:$]+)/.exec(binary);
+  if (!matches)
+    return null;
+  return matches[2];
+};
 
-  parsePort: (binary) => {
-    const matches = /teeworlds_(players|ping)_[^:]+:([0-9]+)$/.exec(binary);
-    if (!matches)
-      return null;
-    return matches[2];
-  },
+module.exports.parsePort = (binary) => {
+  const matches = /teeworlds_(players|ping)_[^:]+:([0-9]+)$/.exec(binary);
+  if (!matches)
+    return null;
+  return matches[2];
+};
 
-  parseTimeout: (env) => {
-    if (env)
-      return parseInt(env);
-    return 10;
-  },
+module.exports.parseTimeout = (env) => {
+  if (env)
+    return parseInt(env);
+  return 5000;
+};
 
-  isConfig: (arg) => {
-    return arg === 'config';
-  },
+module.exports.isConfig = (arg) => {
+  return arg === 'config';
+};
 
+module.exports.waterfall = (callbacks) => {
+  const first = callbacks[0]();
+  return callbacks.slice(1).reduce(function (accumulator, callback) {
+    return accumulator.then(callback);
+  }, first);
 };
